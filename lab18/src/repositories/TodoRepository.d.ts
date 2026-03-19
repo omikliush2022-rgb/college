942b36f81003dbdc4bfdc721443f4c7ade27bf05
unknown <omikliush2022@itcollege..lviv.ua$ git config --global user.email omikliush2022@itcollege.lviv.ua>
2026-03-19 19:41:53 +0100

    lab 17 and 18

---
+++ lab18/src/repositories/TodoRepository.d.ts
@@ -0,0 +1,14 @@
+import { Todo } from "../models/Todo";
+export interface TodoRepository {
+    getAll(): Promise<Todo[]>;
+    add(todo: Todo): Promise<Todo>;
+    update(id: string, data: Partial<Todo>): Promise<Todo>;
+    remove(id: string): Promise<void>;
+}
+export declare class ApiTodoRepository implements TodoRepository {
+    private apiUrl;
+    getAll(): Promise<Todo[]>;
+    add(todo: Todo): Promise<Todo>;
+    update(id: string, data: Partial<Todo>): Promise<Todo>;
+    remove(id: string): Promise<void>;
+}
